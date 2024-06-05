e1d7809f9410d6a1b5299753895e3e90841c1801
zakuri4412 <122138557+zakuri4412@users.noreply.github.com>
2024-06-05 08:01:20 +0700

    Initial commit

---
+++ djangocalendar/cal/views.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta, date
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+import calendar
+
+from .models import *
+from .utils import Calendar
+from .forms import EventForm
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'cal/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('cal:calendar'))
+    return render(request, 'cal/event.html', {'form': form})
