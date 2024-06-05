5d151f9ba7475dcd608c27de3a936d5eceaaabc0
zakuri4412 <122138557+zakuri4412@users.noreply.github.com>
2024-06-05 20:55:56 +0700

    update delete icon

--- djangocalendar/cal/utils.py
+++ djangocalendar/cal/utils.py
@@ -14,10 +14,9 @@ class Calendar(HTMLCalendar):
 		events_per_day = events.filter(start_time__day=day)
 		d = ''
 		for event in events_per_day:
-			d += f'<li> {event.get_html_url} </li>'
-
+			d += f"<li> {event.get_html_url} <i class='bi bi-trash'></i></li>"
 		if day != 0:
-			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
+			return f"<td><span class='date'>{day}</span><ul> {d} </ul> </td>"
 		return '<td></td>'
 
 	# formats a week as a tr
